355e73b413b80c8601c8673aff1f8a37ffb47677
Andy Bleach <ableach@siteset.co.uk>
2021-01-03 16:18:56 +0000

    collect player turn and store

--- tictactoe.c
+++ tictactoe.c
@@ -8,13 +8,27 @@ Date: 03/01/2021
 
 void clearScreen(void);
 void drawGrid(int grid[]);
-void makeMove(int player, int position);
-bool validateMove(void);
-int checkForWin(void);
+int captureMove(int player);
+bool validMove(int playerMove);
+void updateGrid(int grid[], int playerTurn, int playerMove);
+int winnerFound(void);
 
 int main() {
     int grid[10] = {0};
-    drawGrid(grid);
+    int playerTurn = 1, playerMove = 0;
+    bool validity;
+    for ( int i=1; i<10; i++ ) {
+        drawGrid(grid);
+        do {
+            playerMove = captureMove(playerTurn);
+        } while ( validMove(playerMove) == false );
+        updateGrid(grid,playerTurn,playerMove);
+        if ( playerTurn == 1 )
+            playerTurn = 2;
+        else
+            playerTurn = 1;
+    }
+
     return 0;
 }
 
@@ -57,9 +71,15 @@ void clearScreen() {
 }
 
 /*
-store player's move
+capture player's move
 */
-void makeMove(int player, int position) {
+int captureMove(int player) {
+    int playerMove;
+
+    printf("\nPlayer %i - please choose where to play next: ",player);
+    scanf("%i", &playerMove);
+
+    return playerMove;
 }
 
 /*
@@ -67,14 +87,22 @@ check that a move is valid
 1. is position valid (i.e. between 1 and 9)
 2. is position available
 */
-bool validateMove(void) {
-    return false;
+bool validMove(int playerMove) {
+    bool validity = false;
+    if (playerMove >= 1 && playerMove <= 9) {
+        validity = true;
+    }
+    return validity;
+}
+
+void updateGrid(int grid[], int playerTurn, int playerMove) {
+    grid[playerMove] = playerTurn;
 }
 
 /*
 determine if a player has won
 return the player id (1 or 2)
 */
-int checkForWin(void) {
+int winnerFound(void) {
     return 0;
 }

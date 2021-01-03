dadf0ef5f0cd8c08ad19b7e883ecc2d0bf73bddf
Andy Bleach <ableach@siteset.co.uk>
2021-01-03 11:33:46 +0000

    define core functions

--- tictactoe.c
+++ tictactoe.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void drawGrid(void);
+void makeMove(int player, int position);
+bool validateMove(void);
+int checkForWin(void);
 
 int main() {
+    return 0;
+}
+
+/*
+draw the grid
+1. clear screen
+2. header showing which player is which symbol
+3. draw the grid
+*/
+void drawGrid(void) {
+}
+
+/*
+store player's move
+*/
+void makeMove(int player, int position) {
+}
+
+/*
+check that a move is valid
+1. is position valid (i.e. between 1 and 9)
+2. is position available
+*/
+bool validateMove(void) {
+    return false;
+}
+
+/*
+determine if a player has won
+return the player id (1 or 2)
+*/
+int checkForWin(void) {
     return 0;
 }
